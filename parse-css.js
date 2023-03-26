99f2d3eeff3f627fc464d59cdef5e80b22c2119e
Jeremy Rose <jeremy.r.rose@gmail.com>
2023-03-26 16:42:46 -0400

    solution with .reduce

--- parse-css.js
+++ parse-css.js
@@ -1,19 +1,25 @@
 // this function is exported and used in index.js for testing
 export default function parseCSS (cssString) {
 
-    const lines = cssString.split(";")
+    // split the string into an array of lines
+    return cssString.split(";")
+                // then "reduce" the array
+                // "acc" is the accumulator, which is returned after the loop
+                // "cur" is the current value (so each line)
+                // whatever we return from the callback becomes the accumulator for the next loop!
+                .reduce((acc,cur) => {
+                    // split the current line into "property" and "value"
+                    const [property, value] = cur.split(":")
+                    // guard against empty or incomplete lines
+                    if (property && value) {
+                        // set the key and value in our accumulator
+                        acc[property.trim()] = value.trim()
+                    }
+                    // return the accumulator for the next loop
+                    return acc
+                // IMPORTANT! the second argument for .reduce (the part after the comma)
+                // sets the initial value for the accumulator
+                // here it's {}, so we start with an empty object    
+                },{})
 
-    const objToReturn = {}
-
-    lines.forEach(line => {
-
-        const [key, value] = line.split(":")
-        
-        if (key && value) {
-            objToReturn[key.trim()] = value.trim()
-        }
-    })
-
-    // change the code here and make it return the correct object!
-    return objToReturn
 }
