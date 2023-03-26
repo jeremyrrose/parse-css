c60f7e0c00cad2e0b3b2a141dc921df64ab8d752
Jeremy Rose <jeremy.r.rose@gmail.com>
2023-03-26 16:46:25 -0400

    comments

--- parse-css.js
+++ parse-css.js
@@ -1,19 +1,28 @@
 // this function is exported and used in index.js for testing
 export default function parseCSS (cssString) {
 
+    // split the string into an array of lines
     const lines = cssString.split(";")
 
+    // create our empty object
     const objToReturn = {}
 
+    // loop through the lines
     lines.forEach(line => {
 
-        const [key, value] = line.split(":")
+        // split each line at the colon
+        // store the first part as "property", the second part as "value"
+        const [property, value] = line.split(":")
         
-        if (key && value) {
-            objToReturn[key.trim()] = value.trim()
+        // guard against empty or incomplete lines
+        if (property && value) {
+
+            // use .trim to get rid of newlines and whitespace
+            // use our "property" variable to set a new key in our object
+            // the value is from our "value" variable
+            objToReturn[property.trim()] = value.trim()
         }
     })
 
-    // change the code here and make it return the correct object!
     return objToReturn
 }
