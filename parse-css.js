42872f84d3627372851a48b4e6883cc925c1736e
Jeremy Rose <jeremy.r.rose@gmail.com>
2023-03-26 16:33:13 -0400

    verbose solution w/ comments

--- parse-css.js
+++ parse-css.js
@@ -1,6 +1,19 @@
 // this function is exported and used in index.js for testing
 export default function parseCSS (cssString) {
 
+    const lines = cssString.split(";")
+
+    const objToReturn = {}
+
+    lines.forEach(line => {
+
+        const [key, value] = line.split(":")
+        
+        if (key && value) {
+            objToReturn[key.trim()] = value.trim()
+        }
+    })
+
     // change the code here and make it return the correct object!
-    return cssString
+    return objToReturn
 }
